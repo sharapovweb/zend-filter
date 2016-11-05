ee98c252964a1213d581d97983fcc00da587a4ab
Alexander <alexander@sharapov.biz>
2016-11-05 22:06:31 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# zend-filter
+A batch of additional filters for zend framework (https://framework.zend.com)
+
+Installation (via Composer):
+============================
+
+For composer installation, add:
+
+```json
+"require": {
+    "sharapov/zend-filter": "dev-master"
+},
+```
+
+to your composer.json file and update your dependencies. Or you can run:
+
+```sh
+$ composer require sharapov/zend-filter
+```
+
+Usage:
+======
+
+Now you can autoload or use the class via its namespace.
+
+Changelog
+=========
+
+
+Links
+=====
+[ZendFramework](https://github.com/zendframework)
+
+[Composer](https://getcomposer.org/)
+
+[GitHub](https://github.com/sharapovweb/zend-filter)
+
+[Packagist](https://packagist.org/packages/sharapov/zend-filter)
---
+++ src/zend-filter/I18n/AlnumDash.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/zf2 for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Sharapov\ZendFilter\I18n;
+
+use Locale;
+
+class AlnumDash extends \Zend\I18n\Filter\Alnum
+{
+  /**
+   * @var array
+   */
+  protected $options = [
+      'locale'            => null,
+      'allow_white_space' => false,
+      'allow_dashes'      => false,
+      'allow_underscores' => false
+  ];
+
+  /**
+   * Sets the allow_dashes option
+   *
+   * @param  bool $flag
+   * @return AlnumDash Provides a fluent interface
+   */
+  public function setAllowDashes($flag = true)
+  {
+    $this->options['allow_dashes'] = (bool) $flag;
+    return $this;
+  }
+
+  /**
+   * Whether dashes is allowed
+   *
+   * @return bool
+   */
+  public function getAllowDashes()
+  {
+    return $this->options['allow_dashes'];
+  }
+
+  /**
+   * Sets the allow_underscores option
+   *
+   * @param  bool $flag
+   * @return AlnumDash Provides a fluent interface
+   */
+  public function setAllowUnderscores($flag = true)
+  {
+    $this->options['allow_underscores'] = (bool) $flag;
+    return $this;
+  }
+
+  /**
+   * Whether underscores is allowed
+   *
+   * @return bool
+   */
+  public function getAllowUnderscores()
+  {
+    return $this->options['allow_underscores'];
+  }
+
+  /**
+   * Defined by Zend\Filter\FilterInterface
+   *
+   * Returns $value as string with all non-alphanumeric, dash and underscore characters removed
+   *
+   * @param  string|array $value
+   * @return string|array
+   */
+  public function filter($value)
+  {
+    if (!is_scalar($value) && !is_array($value)) {
+      return $value;
+    }
+
+    $whiteSpace = $this->options['allow_white_space'] ? '\s' : '';
+    $dashes = $this->options['allow_dashes'] ? '-' : '';
+    $underscores = $this->options['allow_underscores'] ? '-' : '';
+    $language   = Locale::getPrimaryLanguage($this->getLocale());
+
+    if (!static::hasPcreUnicodeSupport()) {
+      // POSIX named classes are not supported, use alternative a-zA-Z0-9 match
+      $pattern = '/[^a-zA-Z0-9' . $whiteSpace . $dashes . $underscores . ']/';
+    } elseif ($language == 'ja'|| $language == 'ko' || $language == 'zh') {
+      // Use english alphabet
+      $pattern = '/[^a-zA-Z0-9'  . $whiteSpace . $dashes . $underscores . ']/u';
+    } else {
+      // Use native language alphabet
+      $pattern = '/[^\p{L}\p{N}' . $whiteSpace . $dashes . $underscores . ']/u';
+    }
+
+    return preg_replace($pattern, '', $value);
+  }
+}
